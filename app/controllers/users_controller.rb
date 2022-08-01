818e60ab22e39931573a266eb725f5b1337a2948
EC2 Default User <wakana980830@gmail.com>
2022-08-01 22:30:45 +0000

    投稿からのエラーを修正してくれ

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.new
+    @books = @user.books
+  end
+  
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to:"homes#top"
   devise_for :users
-  get 'books'=>'books#new'
+  
+  resources :books
+  resources :users, only: [:show, :edit]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
