42c46da271bdf954991872ff7dd1903d4eb8da78
EC2 Default User <wakana980830@gmail.com>
2022-08-03 22:34:38 +0000

    投稿機能の作成おけい？

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to:'homes#top'
   devise_for:users
-  get "/homes/about" => "homes#about"
+  get "/homes/about" => "homes#about",as:"about"
   resources:books
   resources:users
 
