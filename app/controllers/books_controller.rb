94160ef5a5120951f5c1ad7e92b1215cad952a02
EC2 Default User <wakana980830@gmail.com>
2022-08-05 12:36:48 +0000

    プロフ詳細ページ完成させよう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  def new
+  def index
     @book = Book.new
   end
   
@@ -8,7 +8,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path
+  end
+  
+  def show
+   @book = Book.find(params[:id])
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to:'homes#top'
+  #destroy_user_session DELETE /users/sign_out(.:format) devise/sessions#destroy
+  
   devise_for:users
+  delete 'users/sign_out' => 'sessions#destroy'
   get "/homes/about" => "homes#about",as:"about"
   resources:books
-  resources:users
+  get 'books/:id' => 'books#show'
+  resources:users,only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
